0340b8d874a1d89986ab7563d2494d5e2e70ae56
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-09-12 22:05:16 -0300

    testando strapi

---
+++ src/api/disciplina/controllers/disciplina.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * disciplina controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::disciplina.disciplina');
---
+++ src/api/sala/controllers/sala.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sala controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sala.sala');
